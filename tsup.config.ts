77c08cc02ab5e45642b05a546b907632c13e095f
Evaldo Klock <neto.nemesis@gmail.com>
2026-04-13 10:27:15 -0400

    feat: update version to 0.1.7 and enforce Node.js target version to 22

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "bin/deep-health.ts"],
   format: ["esm"],
-  target: "node20",
+  target: "node22",
   clean: true,
   dts: true,
   sourcemap: false,
